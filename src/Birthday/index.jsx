3f8da002e19ea75c2cea2293cc293a080dbe8fc5
akshayk-7 <akshaykokitkar803@gmail.com>
2026-02-15 00:44:17 +0530

    added some animation

--- src/Birthday/index.jsx
+++ src/Birthday/index.jsx
@@ -9,19 +9,25 @@ const bdata=[
     image:"https://imageio.forbes.com/specials-images/imageserve/645ea1c4fce09061884bd21c/0x0.jpg?format=jpg&crop=2774,2772,x925,y0,safe&height=416&width=416&fit=bounds"
   },
   {
-    name:"Christopher Campbell",
-    age:27,
-    image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    name:"Virat Kohli",
+    age:36,
+    image:"https://d13ir53smqqeyp.cloudfront.net/fc-player-images/635.png"
+    
   },
   {
     name:"Albert Dera",
-    age: 30,
+    age: 35,
     image:"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     name:"Frank Flores",
-    age:34,
+    age:39,
     image:"https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    name:"Christopher Campbell",
+    age:28,
+    image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  
   }
 
 ]
