4a872a22c83c6642aac232be52df879281f4f574
Akshay Kokitkar <131384941+akshayk-7@users.noreply.github.com>
2026-02-25 21:57:24 +0530

    Implement theme context and toggle functionality
    Added theme context and functionality to toggle between light and dark themes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React { createContext, useState, useEffect} from 'react'
 import Pixaby from './Pixaby/Test'
 
+const Theme = createContext()
+
 const App = () => {
+  const [theme, setTheme]  = useState('light')
+
+  useEffect(() => {
+    document.body.className = theme
+  },[theme])
+
+  function ChangeTheme() {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
   return (
     <div>
+      <Theme.Provider value={{theme, ChangeTheme}}>
       <Pixaby />
+      </Theme.Provider>
     </div>
   )
 }
 
+export {Theme}
 export default App
